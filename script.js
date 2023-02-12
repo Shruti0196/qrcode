549271dc33bddb6e74c45fc87d7ce78656d5ecaa
Shruti Prasad <80119277+Shruti0196@users.noreply.github.com>
2023-02-12 13:36:59 +0530

    Update script.js

--- script.js
+++ script.js
@@ -7,7 +7,7 @@ const onGenerateSubmit = (e) => {
 
   clearUI();
 
-  const txt="Name : Juice, Qty:100 ml, Price:Rs.100, Manufactured By: M005, Shipped by: M005 to 3rd party seller 2003"
+  const txt="https://opensea.io/assets/ethereum/0x8943c7bac1914c9a7aba750bf2b6b09fd21037e0/8306"
   const size = 100
 
   // Validate url
